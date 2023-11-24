cf0e6d4bf5d3f72adc0ff2feadff31d0d438e32e
Enrique Barrios <EnriqueB5310@gmail.com>
2023-11-24 13:37:03 -0500

    Query for finding flights with open seats

---
+++ src/main/java/com/CSCProject/TicketAPI/apiService/RecieptService.java
@@ -0,0 +1,13 @@
+package com.CSCProject.TicketAPI.apiService;
+
+
+import com.CSCProject.TicketAPI.recieptModel.FlightReceipt;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RecieptService {
+
+    public FlightReceipt getReciept(int id);
+
+
+}
