5209cb98692f6f1a7cfbf6c52b9ef2b3a038ae84
SevanElevan <livemobile369@gmail.com>
2023-11-26 20:15:41 -0500

    Added default constructor and getters and setters.

--- src/main/java/com/CSCProject/TicketAPI/flightmodel/Flight.java
+++ src/main/java/com/CSCProject/TicketAPI/flightmodel/Flight.java
@@ -15,17 +15,69 @@ public class Flight {
 
     @Id
     private int id;
-
     private double price;
-
     private String airline;
-
     private int seats;
-
     private String from;
     private String to;
-
     private String time;
 
+    public Flight() {
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
