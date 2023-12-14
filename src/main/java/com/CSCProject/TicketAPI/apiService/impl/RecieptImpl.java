14bb6bb314d6455d3702a1d524324d03ce060724
Enrique Barrios <EnriqueB5310@gmail.com>
2023-12-14 11:18:09 -0500

    refactoring files

--- src/main/java/com/CSCProject/TicketAPI/apiService/impl/RecieptImpl.java
+++ src/main/java/com/CSCProject/TicketAPI/apiService/impl/RecieptImpl.java
@@ -26,6 +26,7 @@ public class RecieptImpl implements RecieptService {
     }
 
     /**
+     * @param receipt
      * @return
      */
     @Override
--- src/main/java/com/CSCProject/TicketAPI/util/WebConfig.java
+++ src/main/java/com/CSCProject/TicketAPI/util/WebConfig.java
@@ -1,4 +1,4 @@
-package com.CSCProject.TicketAPI;
+package com.CSCProject.TicketAPI.util;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
